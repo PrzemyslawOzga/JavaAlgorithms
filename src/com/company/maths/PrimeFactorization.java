f1a111180ce649f7121bec002e01641a10a6445e
Przemyslaw Ozga <przemyslaw.ozga6@gmail.com>
2022-03-16 00:53:48 +0100

    New math algorithms.

--- src/com/company/maths/PrimeFactorization.java
+++ src/com/company/maths/PrimeFactorization.java
@@ -1,6 +1,16 @@
+/*
+Factorize the number entered by the user.
+
+Example:
+Input: 30
+Output: 2,3,5 // 2*3*5==30
+*/
+
 package com.company.maths;
 
 public class PrimeFactorization {
+
+
 
     public static void main(String[] args) {
 
