5029a4f7e286aa74035d79f0c7e25ed65e4993dd
Przemyslaw Ozga <przemyslaw.ozga6@gmail.com>
2022-03-16 23:21:43 +0100

    New math algorithms - prime factorization.

--- src/com/company/maths/PrimeFactorization.java
+++ src/com/company/maths/PrimeFactorization.java
@@ -8,11 +8,34 @@ Output: 2,3,5 // 2*3*5==30
 
 package com.company.maths;
 
+import java.util.Scanner;
+
 public class PrimeFactorization {
 
+    public static void primeFactors(int n) {
+        while (n % 2 == 0) {
+            System.out.println(2 + " ");
+            n /= 2;
+        }
 
+        for (int i = 3; i <= Math.sqrt(n); i += 2) {
+            while (n % i == 0) {
+                System.out.println(i + " ");
+                n /= i;
+            }
+        }
+
+        if (n > 2) {
+            System.out.println(n);
+        }
+    }
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a number: ");
+        int n = scanner.nextInt();
+        System.out.println("Printing factors of " + n + ": ");
+        primeFactors(n);
+        scanner.close();
     }
 }
