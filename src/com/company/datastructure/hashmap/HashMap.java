115794659ff9cb0049c95f05498e5a8459e4b520
Przemyslaw Ozga <przemyslaw.ozga6@gmail.com>
2022-03-21 21:43:29 +0100

    Hashmap data structure.

---
+++ src/com/company/datastructure/hashmap/HashMap.java
@@ -0,0 +1,159 @@
+package com.company.datastructure.hashmap;
+
+import java.util.LinkedList;
+
+public class HashMap {
+
+    /*
+    HashMap stores elements in so-called buckets and the number of buckets is called capacity.
+    When we put a value in the map, the key's hashCode() method is used to determine the
+    bucket in which the value will be stored. To retrieve the value, HashMap calculates the
+    bucket in the same way – using hashCode().
+    */
+
+    private int hashSize;
+    private LinkedList[] buckets;
+
+    public HashMap(int hashSize) {
+        buckets = new LinkedList[hashSize];
+        for (int i = 0; i < hashSize; i++)  {
+            buckets[i] = new LinkedList();
+            // In Java, we need to explicitly initialize each object.
+        }
+    }
+
+    public int hashing(int key) {
+        int hash = key % hashSize;
+        if (hash < 0) {
+            hash += hashSize;
+        }
+        return hash;
+    }
+
+    public void insertHash (int key) {
+        int hash = hashing(key);
+        buckets[hash].insert(key);
+    }
+
+    public void deleteHash(int key) {
+        int hash = hashing(key);
+
+        buckets[hash].delete(key);
+    }
+
+    public void displayHashtable() {
+        for (int i = 0; i < hashSize; i++) {
+            System.out.printf("Bucket %d :", i);
+            System.out.println(buckets[i].display());
+        }
+    }
+
+    public static class LinkedList {
+
+        private Node first;
+
+        public LinkedList() {
+            first = null;
+        }
+
+        public void insert(int key) {
+            if (isEmpty()) {
+                first = new Node(key);
+                return;
+            }
+
+            Node temp = findEnd(first);
+            temp.setNext(new Node(key));
+        }
+
+        private Node findEnd(Node n) {
+            if (n.getNext() == null) {
+                return n;
+            } else {
+                return findEnd(n.getNext());
+            }
+        }
+
+        public Node findKey(int key) {
+            if (!isEmpty()) {
+                return findKey(first, key);
+            } else {
+                System.out.println("List is empty");
+                return null;
+            }
+        }
+
+        private Node findKey(Node n, int key) {
+            if (n.getKey() == key) {
+                return n;
+            } else if (n.getNext() == null) {
+                System.out.println("Key not found");
+                return null;
+            } else {
+                return findKey(n.getNext(), key);
+            }
+        }
+
+        public void delete(int key) {
+            if (!isEmpty()) {
+                if (first.getKey() == key) {
+                    first = null;
+                } else {
+                    delete(first, key);
+                }
+            } else {
+                System.out.println("List is empty");
+            }
+        }
+
+        private void delete(Node n, int key) {
+            if (n.getNext().getKey() == key) {
+                if (n.getNext().getNext() == null) {
+                    n.setNext(null);
+                } else {
+                    n.setNext(n.getNext().getNext());
+                }
+            }
+        }
+
+        public String display() {
+            return display(first);
+        }
+
+        private String display(Node n) {
+            if (n == null) {
+                return "null";
+            } else {
+                return n.getKey() + "->" + display(n.getNext());
+            }
+        }
+
+        public boolean isEmpty() {
+            return first == null;
+        }
+    }
+
+    public static class Node {
+
+        private Node next;
+        private int key;
+
+        public Node(int key) {
+            next = null;
+            this.key = key;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public int getKey() {
+            return key;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+    }
+
+}
---
+++ src/com/company/datastructure/hashmap/HashMapLinearProbing.java
@@ -0,0 +1,159 @@
+package com.company.datastructure.hashmap;
+
+import java.util.*;
+
+public class HashMapLinearProbing {
+
+    /*
+    hashSize - size of the hash table
+    buckets - array representing the table
+    size - amount of elements in the hash table
+    */
+
+    private int hashSize;
+    private Integer[] buckets;
+    private Integer AVAILABLE;
+    private int size;
+
+    // Constructor
+    public HashMapLinearProbing(int hashSize) {
+        this.buckets = new Integer[hashSize];
+        this.hashSize = hashSize;
+        this.AVAILABLE = Integer.MIN_VALUE;
+        this.size = 0;
+    }
+
+    //
+    public int hashing(int key) {
+        int hash = key % hashSize;
+        if (hash < 0) {
+            hash += hashSize;
+        }
+        return hash;
+    }
+
+    public void insertHash(int key) {
+        Integer wrappedInt = key;
+        int hash = hashing(key);
+
+        if (isFull()) {
+            System.out.println("Hash table is full");
+            return;
+        }
+
+        for (int i = 0; i < hashSize; i++) {
+            if (buckets[hash] == null || buckets[hash] == AVAILABLE) {
+                buckets[hash] = wrappedInt;
+                size++;
+                return;
+            }
+
+            if (hash + 1 < hashSize) {
+                hash++;
+            } else {
+                hash = 0;
+            }
+        }
+    }
+
+    public void deleteHash(int key) {
+        Integer wrappedInt = key;
+        int hash = hashing(key);
+
+        if (isEmpty()) {
+            System.out.println("Table is empty");
+            return;
+        }
+
+        for (int i = 0; i < hashSize; i++) {
+            if (buckets[hash] != null && buckets[hash].equals(wrappedInt)) {
+                buckets[hash] = AVAILABLE;
+                size--;
+                return;
+            }
+
+            if (hash + 1 < hashSize) {
+                hash++;
+            } else {
+                hash = 0;
+            }
+        }
+        System.out.println("Key " + key + " not found");
+    }
+
+    public void displayHashtable() {
+        for (int i = 0; i < hashSize; i++) {
+            if (buckets[i] == null || buckets[i] == AVAILABLE) {
+                System.out.println("Bucket " + i + ": Empty");
+            } else {
+                System.out.println("Bucket " + i + ": " + buckets[i].toString());
+            }
+        }
+    }
+
+    public int findHash(int key) {
+        Integer wrappedInt = key;
+        int hash = hashing(key);
+
+        if (isEmpty()) {
+            System.out.println("Table is empty");
+            return -1;
+        }
+
+        for (int i = 0; i < hashSize; i++) {
+            try {
+                if (buckets[hash].equals(wrappedInt)) {
+                    buckets[hash] = AVAILABLE;
+                    return hash;
+                }
+            } catch (Exception E) {
+            }
+
+            if (hash + 1 < hashSize) {
+                hash++;
+            } else {
+                hash = 0;
+            }
+        }
+        System.out.println("Key " + key + " not found");
+        return -1;
+    }
+
+    private void lengthenTable() {
+        buckets = Arrays.copyOf(buckets, hashSize * 2);
+        hashSize *= 2;
+        System.out.println("Table size is now: " + hashSize);
+    }
+
+    public void checkLoadFactor() {
+        double factor = (double) size / hashSize;
+        if (factor > .7) {
+            System.out.println("Load factor is " + factor + ",  lengthening table");
+            lengthenTable();
+        } else {
+            System.out.println("Load factor is " + factor);
+        }
+    }
+
+    public boolean isFull() {
+        boolean response = true;
+        for (int i = 0; i < hashSize; i++) {
+            if (buckets[i] == null || buckets[i] == AVAILABLE) {
+                response = false;
+                break;
+            }
+        }
+        return response;
+    }
+
+    public boolean isEmpty() {
+        boolean response = true;
+        for (int i = 0; i < hashSize; i++) {
+            if (buckets[i] != null) {
+                response = false;
+                break;
+            }
+        }
+        return response;
+    }
+}
