825575da589d7159fba1f9c7193cf823506c83ca
Przemyslaw Ozga <przemyslaw.ozga6@gmail.com>
2022-04-01 01:06:32 +0200

    Math algorithm using lists - remove element from array. Complexity: O(n)

--- src/com/company/datastructure/removeDuplicates.java
+++ src/com/company/datastructure/removeDuplicates.java
@@ -32,7 +32,7 @@ public class removeDuplicates {
 
     public static void main(String[] args) {
         int[] nums = {0,0,1,1,1,2,2,3,3,4};
-        int[] nums1 = {1,1,2};
+        int[] nums1 = {1,1,2,3,3,4};
 
         System.out.println(removeDuplicates(nums));
         System.out.println();
---
+++ src/com/company/datastructure/removeElement.java
@@ -0,0 +1,43 @@
+package com.company.datastructure;
+
+/*
+Problem: Remove Element
+Example: Input: nums = [0,1,2,2,3,0,4,2], val = 2
+         Output: 5, nums = [0,1,4,0,3,_,_,_]
+*/
+
+public class removeElement {
+
+    // Complexity: O(n)
+    public static int removeElement(int[] nums, int val) {
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+
+        for (int k = 0; k < nums.length; k++) {
+            System.out.print(nums[k] + " ");
+        }
+        System.out.println();
+
+        return i;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0,1,2,2,3,0,4,2};
+        int val = 2;
+
+        int[] nums1 = {3,2,2,3};
+        int val1 = 3;
+
+        System.out.println(removeElement(nums, val));
+
+    }
+}
