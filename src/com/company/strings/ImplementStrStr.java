2f54566e9b01373308829c555bf09d3bf7d7dd4e
Przemyslaw Ozga <przemyslaw.ozga6@gmail.com>
2022-04-04 00:53:44 +0200

    Implement strStr() - letCode algorithm.

--- src/com/company/strings/ImplementStrStr.java
+++ src/com/company/strings/ImplementStrStr.java
@@ -1,22 +1,33 @@
 package com.company.strings;
 
+
 public class ImplementStrStr {
     public static int strStr(String haystack, String needle) {
-        char ch[] = haystack.toCharArray();
 
-        for (int i = 0; i < ch.length ; i++)  {
-            System.out.println(ch[i]);
+        if (needle.isEmpty()) {
+            return 0;
+        } else if (needle.length() > haystack.length()) {
+            return -1;
         }
 
-        return 0;
+        for (int i = 0; i < haystack.length() - needle.length() + 1; i++) {
+            if (haystack.charAt(i) == needle.charAt(0)){
+                String substring = haystack.substring(i, i + needle.length());
+                if (substring.equals(needle))
+                    return i;
+            }
+        }
+
+        return -1;
     }
 
     public static void main(String[] args) {
         String haystack = "hello";
-        String haestack1 = "aaaaa";
         String needle = "ll";
-        String needle1 = "bba";
+        String haystack1 = "mississippi";
+        String needle1 = "issip";
 
-        strStr(haystack, needle);
+        System.out.println(strStr(haystack, needle));
+        System.out.println(strStr(haystack1, needle1));
     }
 }
