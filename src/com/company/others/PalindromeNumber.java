73259dafe7eb1a877841e339310cb744a7f3fb7e
Przemyslaw Ozga <przemyslaw.ozga6@gmail.com>
2022-04-04 21:37:55 +0200

    New Java algorithm - palindrome numbers (letcode).

--- src/com/company/others/PalindromeNumber.java
+++ src/com/company/others/PalindromeNumber.java
@@ -1,13 +1,37 @@
 package com.company.others;
 
+/*
+Problem: Palindrome Number: given an integer x/y, return true if x/y is palindrome integer.
+Example: Input: x = 121, y = -121
+         Output: true, false
+*/
+
 public class PalindromeNumber {
-    public boolean isPalindrome(int x) {
+    public static boolean isPalindrome(int x) {
+        String string = Integer.toString(x);
+        char[] ch = string.toCharArray();
 
 
-        return false;
+        int i = 0;
+        for (int j = ch.length - 1; j >= 0; j--) {
+            if (ch[i] != ch[j]) {
+                return false;
+            }
+            i++;
+        }
+
+        return true;
     }
 
     public static void main(String[] args) {
+        int x = 1000021; // false
+        int y = 121; // true
+        int z = 10;  // false
+        int a = -121; // false
 
+        System.out.println(isPalindrome(x));
+        System.out.println(isPalindrome(y));
+        System.out.println(isPalindrome(z));
+        System.out.println(isPalindrome(a));
     }
 }
