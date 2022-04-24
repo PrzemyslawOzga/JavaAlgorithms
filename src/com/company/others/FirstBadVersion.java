322a78ab665eda727220c8e0a6065ebca49881c5
Przemyslaw Ozga <przemyslaw.ozga6@gmail.com>
2022-04-24 21:08:11 +0200

    New letcode algorithm #278 - first bad version.

--- src/com/company/others/FirstBadVersion.java
+++ src/com/company/others/FirstBadVersion.java
@@ -17,6 +17,7 @@ public class FirstBadVersion {
     public int firstBadVersion(int n) {
         int l = 1, r = n;
 
+        /*
         while (l < r) {
             int mid = l + (r - l) / 2;
             if (!isBadVersion(mid)) {
@@ -26,6 +27,7 @@ public class FirstBadVersion {
             }
         }
 
+        */
         return l;
     }
 }
