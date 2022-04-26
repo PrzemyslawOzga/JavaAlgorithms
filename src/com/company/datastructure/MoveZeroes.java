1d8e516d3582e6fdd11dfaeda347b406c4803539
Przemyslaw Ozga <przemyslaw.ozga6@gmail.com>
2022-04-26 21:10:50 +0200

    New letcode algorithm - move zeroes.

---
+++ src/com/company/datastructure/MoveZeroes.java
@@ -0,0 +1,26 @@
+package com.company.datastructure;
+
+/*
+Problem: move zeroes
+Description: given an array nums[], move all 0's to the end of it while maintaining the relative
+order of the non-zero elements. Note that you must do thin in-place without making a copy of the array.
+*/
+
+public class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        int n = -1;
+        for (int i = 0; i < nums.length; i++) {
+            int tmp = nums[i];
+            if (tmp != 0) {
+                n++;
+                nums[i] = nums[n];
+                nums[n] = tmp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {0,1,0,3,12};
+        int[] nums2 = {0};
+    }
+}
